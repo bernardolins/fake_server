23ec777296a511b5107ba13e0bcd877d15f2dcc8
bernardolins <bernardolins28@gmail.com>
2016-09-08 00:07:13 -0300

    Create function to to add a new behavior

--- lib/behavior/behavior.ex
+++ lib/behavior/behavior.ex
@@ -2,4 +2,14 @@ defmodule Fakex.Behavior do
   def begin do
     Agent.start_link(fn -> [] end, name: __MODULE__)
   end
+
+  def add(name, behavior = [code: _code, body: _body]) do
+    case is_atom name do
+      true -> Agent.update(__MODULE__, fn(behavior_list) -> Keyword.put(behavior_list, name, behavior) end)
+      false -> {:error, :invalid_name}
+    end
+  end
+  def add(_behavior) do
+    {:error, :invalid_name}
+  end
 end
--- test/behavior/behavior_test.exs
+++ test/behavior/behavior_test.exs
@@ -6,4 +6,15 @@ defmodule Fakex.BehaviorTest do
     Fakex.Behavior.begin
     assert Agent.get(Fakex.Behavior, fn(list) -> list end) == []
   end
+
+  test "#add return error if name is not atom" do
+    assert Fakex.Behavior.add("some_invalid_name", code: 200, body: ~s<"user": "test">) == {:error, :invalid_name}
+  end
+
+  test "#add returns error if name not provided" do
+    assert Fakex.Behavior.add(code: 200) == {:error, :invalid_name}
+  end
+
+  test "#add put a new behavior on the list" do
+  end
 end
