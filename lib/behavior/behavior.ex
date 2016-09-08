ba8c0605f8c8c330c5a5e5e34992046277508c85
bernardolins <bernardolins28@gmail.com>
2016-09-08 00:55:57 -0300

    Rename Fakex.Behavior#add to Fakex.Behavior#create

--- lib/behavior/behavior.ex
+++ lib/behavior/behavior.ex
@@ -3,19 +3,19 @@ defmodule Fakex.Behavior do
     Agent.start_link(fn -> [] end, name: __MODULE__)
   end
 
-  def add(name, behavior = %{response_code: _code, response_body: _body}) do
+  def create(name, behavior = %{response_code: _code, response_body: _body}) do
     case is_atom name do
       true -> Agent.update(__MODULE__, fn(behavior_list) -> Keyword.put(behavior_list, name, behavior) end)
       false -> {:error, :invalid_name}
     end
   end
-  def add(name, %{response_body: _body}) do
+  def create(name, %{response_body: _body}) do
     {:error, :response_code_not_provided}
   end
-  def add(name, %{response_code: _code}) do
+  def create(name, %{response_code: _code}) do
     {:error, :response_body_not_provided}
   end
-  def add(_behavior) do
+  def create(_behavior) do
     {:error, :name_not_provided}
   end
 end
--- test/behavior/behavior_test.exs
+++ test/behavior/behavior_test.exs
@@ -10,26 +10,27 @@ defmodule Fakex.BehaviorTest do
     Agent.stop(Fakex.Behavior)
   end
 
-  test "#add return error if name is not atom" do
-    assert Fakex.Behavior.add("some_invalid_name", @valid_behavior) == {:error, :invalid_name}
+  test "#create return error if name is not atom" do
+    assert Fakex.Behavior.create("some_invalid_name", @valid_behavior) == {:error, :invalid_name}
   end
 
-  test "#add returns error if name not provided" do
-    assert Fakex.Behavior.add(@valid_behavior) == {:error, :name_not_provided}
+  test "#create returns error if name not provided" do
+    assert Fakex.Behavior.create(@valid_behavior) == {:error, :name_not_provided}
   end
 
-  test "#add returns error if response_body not provided" do
-    assert Fakex.Behavior.add(:test, %{response_code: 200}) == {:error, :response_body_not_provided}
+  test "#create returns error if response_body not provided" do
+    assert Fakex.Behavior.create(:test, %{response_code: 200}) == {:error, :response_body_not_provided}
   end
 
-  test "#add returns error if response_code not provided" do
-    assert Fakex.Behavior.add(:test, %{response_body: ~s<"user": "test">}) == {:error, :response_code_not_provided}
+  test "#create returns error if response_code not provided" do
+    assert Fakex.Behavior.create(:test, %{response_body: ~s<"user": "test">}) == {:error, :response_code_not_provided}
   end
 
-  test "#add put a new behavior on the list" do
+  test "#create put a new behavior on the list" do
     Fakex.Behavior.begin
     assert Agent.get(Fakex.Behavior, fn(list) -> list end) == []
-    Fakex.Behavior.add(:test, @valid_behavior) == {:error, :response_code_not_provided}
+    Fakex.Behavior.create(:test, @valid_behavior) == {:error, :response_code_not_provided}
     assert Agent.get(Fakex.Behavior, fn(list) -> list end) == [test: @valid_behavior]
+    Agent.stop(Fakex.Behavior)
   end
 end
