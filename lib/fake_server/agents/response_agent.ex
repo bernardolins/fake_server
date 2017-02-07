40ee08eed4c6d72270e7fe500f9672a5621f7fdc
bernardolins <bernardolins28@gmail.com>
2017-02-07 00:13:30 -0200

    Version 1.0.0: Create basic response agent to store response state

---
+++ lib/fake_server/agents/response_agent.ex
@@ -0,0 +1,24 @@
+defmodule FakeServer.Agents.ResponseAgent do
+  def start_link do
+    Agent.start_link(fn -> [] end, name: __MODULE__)
+  end
+
+  def stop do
+    Agent.stop(__MODULE__)
+  end
+
+  def put_response_list(responses) do
+    Agent.update(__MODULE__, fn(_) -> List.wrap(responses) end)
+  end
+  
+  def take_next do
+    case Agent.get(__MODULE__, fn(responses) -> responses end) do
+      [] -> 
+        FakeServer.HTTP.Response.default()
+      [response|responses] -> 
+        Agent.update(__MODULE__, fn(_) -> responses end)
+        response
+    end
+  end
+
+end
