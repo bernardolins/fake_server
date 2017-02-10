e5883dffdfffd6c29a4e763791931199f9703e85
bernardolins <bernardo.lins@corp.globo.com>
2017-02-09 22:11:25 -0200

    Fix server integration tests

--- test/fake_server/http/server_integration_test.exs
+++ test/fake_server/http/server_integration_test.exs
@@ -1,4 +1,4 @@
-defmodule FakeServer.HTTP.ServerTest do 
+defmodule FakeServer.HTTP.ServerTest do
   use ExUnit.Case
 
   alias FakeServer.Agents.RouterAgent
@@ -15,31 +15,26 @@ defmodule FakeServer.HTTP.ServerTest do
   end
 
   describe "On integration tests" do
-    test "server can be started on a random" do
+    test "server will return 404 on any route if started on a random port without routes" do
       {:ok, port} = FakeServer.HTTP.Server.run
-      assert :ranch_tcp.listen(ip: {127,0,0,1}, port: port) == {:error, :eaddrinuse}
+      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], [])
+      assert response |> elem(0) |> elem(1) == 404
       FakeServer.HTTP.Server.stop
     end
 
-    test "server can be started on a given port" do
+    test "server will return 404 on any route if started on a given port without routes" do
       {:ok, port} = FakeServer.HTTP.Server.run([port: 51289])
       assert port == 51289
-      assert :ranch_tcp.listen(ip: {127,0,0,1}, port: 51289) == {:error, :eaddrinuse}
+      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], [])
+      assert response |> elem(0) |> elem(1) == 404
       FakeServer.HTTP.Server.stop
     end
 
-    test "server can be accessed on a valid route responding default response" do
+    test "server will respond the default response if a route is provided" do
       RouterAgent.put_route("/test")
       {:ok, port} = FakeServer.HTTP.Server.run
-      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], []) 
+      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], [])
       assert response |> elem(0) |> elem(1) == 200
-      FakeServer.HTTP.Server.stop
-    end
-
-    test "server can return 404 if an invalid route is accessed" do
-      {:ok, port} = FakeServer.HTTP.Server.run
-      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], []) 
-      assert response |> elem(0) |> elem(1) == 404 
       FakeServer.HTTP.Server.stop
     end
 
@@ -48,10 +43,10 @@ defmodule FakeServer.HTTP.ServerTest do
       ResponseAgent.put_response_list([Response.forbidden, Response.bad_request])
       {:ok, port} = FakeServer.HTTP.Server.run
 
-      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], []) 
-      assert response |> elem(0) |> elem(1) == 403 
+      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], [])
+      assert response |> elem(0) |> elem(1) == 403
 
-      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], []) 
+      {:ok, response} = :httpc.request(:get, {'http://127.0.0.1:#{port}/test', [{'connection', 'close'}]}, [], [])
       assert response |> elem(0) |> elem(1) == 400
       FakeServer.HTTP.Server.stop
     end
