e3a80a9f61bb2dd6fe7fc0f3fef9975bf8a98f86
bernardolins <bernardolins28@gmail.com>
2017-10-30 10:55:33 -0200

    Set minimal Elixir version to 1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule FakeServer.Mixfile do
   def project do
     [app: :fake_server,
      version: "1.0.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      description: description(),
      package: package(),
      aliases: aliases(),
