2e0ce5e13a072b3fde85c7547735769e238d5131
bernardolins <bernardolins28@gmail.com>
2017-11-06 15:15:12 -0200

    Update to 1.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FakeServer.Mixfile do
 
   def project do
     [app: :fake_server,
-     version: "1.1.0",
+     version: "1.2.0",
      elixir: "~> 1.4",
      description: description(),
      package: package(),
