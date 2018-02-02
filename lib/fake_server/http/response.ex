553f829836c47b51bc1a3c8a3f015683b9cb3bc6
bernardolins <bernardolins28@gmail.com>
2018-02-02 09:13:49 -0200

    Improve Response module docs

--- lib/fake_server/http/response.ex
+++ lib/fake_server/http/response.ex
@@ -27,22 +27,60 @@ defmodule FakeServer.HTTP.Response do
   FakeServer.HTTP.Response.new(404)
   ```
   """
-  def new(status, body \\ "", headers \\ %{}), do: %__MODULE__{code: status, body: body, headers: headers}
+  def new(status_code, body \\ "", headers \\ %{}), do: %__MODULE__{code: status_code, body: body, headers: headers}
 
-  # 2xx
+  @doc """
+  Creates a new response with status code 200
+  """
   def ok(body \\ "", headers \\ %{}), do: new(200, body, headers)
+
+  @doc """
+  Creates a new response with status code 201
+  """
   def created(body \\ "", headers \\ %{}), do: new(201, body, headers)
+
+  @doc """
+  Creates a new response with status code 202
+  """
   def accepted(body \\ "", headers \\ %{}), do: new(202, body, headers)
+
+  @doc """
+  Creates a new response with status code 203
+  """
   def non_authoritative_information(body \\ "", headers \\ %{}), do: new(203, body, headers)
+
+  @doc """
+  Creates a new response with status code 204
+  """
   def no_content(body \\ "", headers \\ %{}), do: new(204, body, headers)
+
+  @doc """
+  Creates a new response with status code 205
+  """
   def reset_content(body \\ "", headers \\ %{}), do: new(205, body, headers)
+
+  @doc """
+  Creates a new response with status code 206
+  """
   def partial_content(body \\ "", headers \\ %{}), do: new(206, body, headers)
+
+  @doc """
+  Creates a new response with status code 207
+  """
   def multi_status(body \\ "", headers \\ %{}), do: new(207, body, headers)
+
+  @doc """
+  Creates a new response with status code 208
+  """
   def already_reported(body \\ "", headers \\ %{}), do: new(208, body, headers)
+
+  @doc """
+  Creates a new response with status code 226
+  """
   def im_used(body \\ "", headers \\ %{}), do: new(226, body, headers)
 
   @doc """
-    Returns a list with all 4xx HTTP methods available
+  Returns a list with all 4xx HTTP methods available
   """
   def all_4xx do
     [
@@ -76,38 +114,144 @@ defmodule FakeServer.HTTP.Response do
     ]
   end
 
-  # 4xx
+  @doc """
+  Creates a new response with status code 400
+  """
   def bad_request(body \\ "", headers \\ %{}), do: new(400, body, headers)
+
+  @doc """
+  Creates a new response with status code 401
+  """
   def unauthorized(body \\ "", headers \\ %{}), do: new(401, body, headers)
+
+  @doc """
+  Creates a new response with status code 402
+  """
   def payment_required(body \\ "", headers \\ %{}), do: new(402, body, headers)
+
+  @doc """
+  Creates a new response with status code 403
+  """
   def forbidden(body \\ "", headers \\ %{}), do: new(403, body, headers)
+
+  @doc """
+  Creates a new response with status code 404
+  """
   def not_found(body \\ "", headers \\ %{}), do: new(404, body, headers)
+
+  @doc """
+  Creates a new response with status code 405
+  """
   def method_not_allowed(body \\ "", headers \\ %{}), do: new(405, body, headers)
+
+  @doc """
+  Creates a new response with status code 406
+  """
   def not_acceptable(body \\ "", headers \\ %{}), do: new(406, body, headers)
+
+  @doc """
+  Creates a new response with status code 407
+  """
   def proxy_authentication_required(body \\ "", headers \\ %{}), do: new(407, body, headers)
+
+  @doc """
+  Creates a new response with status code 408
+  """
   def request_timeout(body \\ "", headers \\ %{}), do: new(408, body, headers)
+
+  @doc """
+  Creates a new response with status code 409
+  """
   def conflict(body \\ "", headers \\ %{}), do: new(409, body, headers)
+
+  @doc """
+  Creates a new response with status code 410
+  """
   def gone(body \\ "", headers \\ %{}), do: new(410, body, headers)
+
+  @doc """
+  Creates a new response with status code 411
+  """
   def length_required(body \\ "", headers \\ %{}), do: new(411, body, headers)
+
+  @doc """
+  Creates a new response with status code 412
+  """
   def precondition_failed(body \\ "", headers \\ %{}), do: new(412, body, headers)
+
+  @doc """
+  Creates a new response with status code 413
+  """
   def payload_too_large(body \\ "", headers \\ %{}), do: new(413, body, headers)
+
+  @doc """
+  Creates a new response with status code 414
+  """
   def uri_too_long(body \\ "", headers \\ %{}), do: new(414, body, headers)
+
+  @doc """
+  Creates a new response with status code 415
+  """
   def unsupported_media_type(body \\ "", headers \\ %{}), do: new(415, body, headers)
+
+  @doc """
+  Creates a new response with status code 417
+  """
   def expectation_failed(body \\ "", headers \\ %{}), do: new(417, body, headers)
+
+  @doc """
+  Creates a new response with status code 418
+  """
   def im_a_teapot(body \\ "", headers \\ %{}), do: new(418, body, headers)
+
+  @doc """
+  Creates a new response with status code 421
+  """
   def misdirected_request(body \\ "", headers \\ %{}), do: new(421, body, headers)
+
+  @doc """
+  Creates a new response with status code 422
+  """
   def unprocessable_entity(body \\ "", headers \\ %{}), do: new(422, body, headers)
+
+  @doc """
+  Creates a new response with status code 423
+  """
   def locked(body \\ "", headers \\ %{}), do: new(423, body, headers)
+
+  @doc """
+  Creates a new response with status code 424
+  """
   def failed_dependency(body \\ "", headers \\ %{}), do: new(424, body, headers)
+
+  @doc """
+  Creates a new response with status code 426
+  """
   def upgrade_required(body \\ "", headers \\ %{}), do: new(426, body, headers)
+
+  @doc """
+  Creates a new response with status code 428
+  """
   def precondition_required(body \\ "", headers \\ %{}), do: new(428, body, headers)
+
+  @doc """
+  Creates a new response with status code 429
+  """
   def too_many_requests(body \\ "", headers \\ %{}), do: new(429, body, headers)
+
+  @doc """
+  Creates a new response with status code 431
+  """
   def request_header_fields_too_large(body \\ "", headers \\ %{}), do: new(431, body, headers)
+
+  @doc """
+  Creates a new response with status code 451
+  """
   def unavailable_for_legal_reasons(body \\ "", headers \\ %{}), do: new(451, body, headers)
 
 
   @doc """
-    Returns a list with all 4xx HTTP methods available
+  Returns a list with all 5xx HTTP methods available
   """
   def all_5xx do
     [
@@ -125,19 +269,64 @@ defmodule FakeServer.HTTP.Response do
     ]
   end
 
-  # 5xx
+  @doc """
+  Creates a new response with status code 500
+  """
   def internal_server_error(body \\ "", headers \\ %{}), do: new(500, body, headers)
+
+  @doc """
+  Creates a new response with status code 501
+  """
   def not_implemented(body \\ "", headers \\ %{}), do: new(501, body, headers)
+
+  @doc """
+  Creates a new response with status code 502
+  """
   def bad_gateway(body \\ "", headers \\ %{}), do: new(502, body, headers)
+
+  @doc """
+  Creates a new response with status code 503
+  """
   def service_unavailable(body \\ "", headers \\ %{}), do: new(503, body, headers)
+
+  @doc """
+  Creates a new response with status code 504
+  """
   def gateway_timeout(body \\ "", headers \\ %{}), do: new(504, body, headers)
+
+  @doc """
+  Creates a new response with status code 505
+  """
   def http_version_not_supported(body \\ "", headers \\ %{}), do: new(505, body, headers)
+
+  @doc """
+  Creates a new response with status code 506
+  """
   def variant_also_negotiates(body \\ "", headers \\ %{}), do: new(506, body, headers)
+
+  @doc """
+  Creates a new response with status code 507
+  """
   def insufficient_storage(body \\ "", headers \\ %{}), do: new(507, body, headers)
+
+  @doc """
+  Creates a new response with status code 508
+  """
   def loop_detected(body \\ "", headers \\ %{}), do: new(508, body, headers)
+
+  @doc """
+  Creates a new response with status code 510
+  """
   def not_extended(body \\ "", headers \\ %{}), do: new(510, body, headers)
+
+  @doc """
+  Creates a new response with status code 511
+  """
   def network_authentication_required(body \\ "", headers \\ %{}), do: new(511, body, headers)
 
+  @doc """
+  FakeServer default response. Used when there are no responses left to reply.
+  """
   def default, do: new(200, ~s<{"message": "This is a default response from FakeServer"}>)
 end
 
