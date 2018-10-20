8baff346bb84a81c50e1b4004e495632a59a54b0
bernardolins <bernardolins28@gmail.com>
2018-10-20 15:59:20 -0300

    Remove old stuff

--- lib/fake_server/application.ex
+++ lib/fake_server/application.ex
@@ -6,10 +6,7 @@ defmodule FakeServer.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    children = [
-      worker(FakeServer.Agents.ServerAgent, []),
-      worker(FakeServer.Agents.EnvAgent, [])
-    ]
+    children = []
 
     opts = [strategy: :one_for_one, name: FakeServer.Supervisor]
     Supervisor.start_link(children, opts)
