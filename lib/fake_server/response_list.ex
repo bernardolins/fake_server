d674981751e72f7a0f3f6bce200ebc12737f1d21
bernardolins <bernardolins28@gmail.com>
2018-10-21 02:15:46 -0200

    Add ResponseList agent

---
+++ lib/fake_server/response_list.ex
@@ -0,0 +1,23 @@
+defmodule FakeServer.ResponseList do
+  @moduledoc false
+
+  def start_link(), do: Agent.start_link(fn -> [] end)
+  def stop(list_id), do: Agent.stop(list_id)
+
+  def add_response(list_id, response) do
+    case FakeServer.Response.validate(response) do
+      :ok ->
+        Agent.update(list_id, &(&1 ++ [response]))
+        :ok
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  def get_next(list_id) do
+    Agent.get_and_update(list_id, fn
+      ([response|responses]) -> {response, responses}
+      ([]) -> {FakeServer.Response.default!, []}
+    end)
+  end
+end
---
+++ test/fake_server/response_list_test.exs
@@ -0,0 +1,37 @@
+defmodule ResponseListTest do
+  use ExUnit.Case
+  alias FakeServer.ResponseList
+  alias FakeServer.Response
+
+  describe "#add_response" do
+    test "returns :ok if a valid response is added" do
+      {:ok, list_id} = ResponseList.start_link
+      assert :ok = ResponseList.add_response(list_id, Response.ok!)
+      ResponseList.stop(list_id)
+    end
+
+    test "returns {:error, reason} if an invalid response is added" do
+      {:ok, list_id} = ResponseList.start_link
+      assert {:error, {1, "invalid response type"}} = ResponseList.add_response(list_id, 1)
+      assert {:error, {600, "invalid status code"}} = ResponseList.add_response(list_id, %Response{code: 600})
+      ResponseList.stop(list_id)
+    end
+  end
+
+  describe "#get_next" do
+    test "returns response by order if there is a response on the list" do
+      {:ok, list_id} = ResponseList.start_link
+      ResponseList.add_response(list_id, Response.ok!)
+      ResponseList.add_response(list_id, Response.forbidden!)
+      assert Response.ok! == ResponseList.get_next(list_id)
+      assert Response.forbidden! == ResponseList.get_next(list_id)
+      ResponseList.stop(list_id)
+    end
+
+    test "returns the default response if the list is empty" do
+      {:ok, list_id} = ResponseList.start_link
+      assert Response.default! == ResponseList.get_next(list_id)
+      ResponseList.stop(list_id)
+    end
+  end
+end
