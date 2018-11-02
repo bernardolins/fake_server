c2a4c0bde0b7566d7443ba7320994a0fc583ac4f
bernardolins <bernardolins28@gmail.com>
2018-11-02 13:05:49 -0200

    Adds helper functions to instance

--- lib/fake_server/server/instance.ex
+++ lib/fake_server/server/instance.ex
@@ -43,8 +43,27 @@ defmodule FakeServer.Instance do
   def stop(server), do: GenServer.stop(server)
   def terminate(_, server), do: FakeServer.Cowboy.stop(server)
 
+  def add_route(server, path, response), do: GenServer.call(server, {:add_route, path, response})
   def access_list(server), do: GenServer.call(server, :access_list)
-  def handle_call(:access_list, _, server), do: {:reply, FakeServer.Server.Access.access_list(server.access), server}
+  def port(server), do: GenServer.call(server, :port)
+
+  def handle_call({:add_route, path, response}, _, server) do
+    case update_router(path, response, server) do
+      {:ok, new_server}         -> {:reply, :ok, new_server}
+      {:error, reason} = error  -> {:reply, error, server}
+    end
+  end
+
+  def handle_call(:access_list, _, server) do
+    case get_access_list(server) do
+      {:ok, access_list} = reply  -> {:reply, reply, server}
+      {:error, reason} = error    -> {:reply, error, server}
+    end
+  end
+
+  def handle_call(:port, _, server) do
+    {:reply, server.port, server}
+  end
 
   defp ensure_server_name(nil), do: {:ok, self()}
   defp ensure_server_name(name) when is_atom(name), do: {:ok, name}
@@ -53,4 +72,23 @@ defmodule FakeServer.Instance do
   defp ensure_max_conn(max_conn) when not is_integer(max_conn), do: {:error, {max_conn, "max_conn must be a positive integer"}}
   defp ensure_max_conn(max_conn) when max_conn < 1, do: {:error, {max_conn, "max_conn must be greater than 0"}}
   defp ensure_max_conn(max_conn), do: {:ok, max_conn}
+
+  defp get_access_list(server) do
+    case FakeServer.Server.Access.access_list(server.access) do
+      access_list when is_list(access_list) -> {:ok, access_list}
+      _ -> {:error, {:access_list, "could not get access list"}}
+    end
+  end
+
+  defp update_router(path, response, server) do
+    with {:ok, route}   <- FakeServer.Route.create(path: path, response: response),
+         routes         <- [route | server.routes],
+         {:ok, router}  <- FakeServer.Router.create(routes, server.access)
+    do
+        :cowboy.set_env(server.server_name, :dispatch, router)
+        {:ok, %__MODULE__{server | routes: routes, router: router}}
+    else
+      error -> {:reply, error, server}
+    end
+  end
 end
