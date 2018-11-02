3b334bf856968da98f49bd44e5e626895ca1111a
bernardolins <bernardolins28@gmail.com>
2018-11-02 13:07:40 -0200

    Starting httpoison on test_helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,7 @@
 ExUnit.configure(exclude: [skip: true])
+
 Application.ensure_all_started(:faker)
 Application.ensure_all_started(:fake_server)
+Application.ensure_all_started(:httpoison)
+
 ExUnit.start()
